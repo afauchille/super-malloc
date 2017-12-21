4cc41c70eec8590d2fd766851132557b7865b6c4
Amaury Fauchille <amaury.fauchille@epita.fr>
2017-12-21 16:41:57 +0100

    Add functions for extracting process id and variable local id  from variable id

--- main.c
+++ main.c
@@ -50,6 +50,12 @@ void simple_recv(int *data, int id_from, int id) // last param only used in prin
  * Variables *
  *************/
 
+static inline
+unsigned long pid_part(unsigned long var_id) { return var_id >> 24; }
+
+static inline
+unsigned long vid_part(unsigned long var_id) { return var_id & 0xFF; }
+
 unsigned long var_alloc(Process *me)
 {
   if (me->size == me->capacity - 1)
@@ -66,14 +72,14 @@ unsigned long var_alloc(Process *me)
 
 int var_get(Process *me, unsigned long var_id)
 {
-  unsigned long i = var_id & 0xFF;
+  unsigned long i = vid_part(var_id);
   if (i > me->size) {
-    fprintf(stderr, "Variable requested with wrong var_id: process %lu, %luth variable\n", var_id >> 24, i);
+    fprintf(stderr, "Variable requested with wrong var_id: process %lu, %luth variable\n", pid_part(var_id), i);
     return -1;
   }
   Variable var = me->data[i];
   if (!var.data) {
-    fprintf(stderr, "Variable requested for already freed vaiable: process %lu, %luth variable\n", var_id >> 24, i);
+    fprintf(stderr, "Variable requested for already freed vaiable: process %lu, %luth variable\n", pid_part(var_id), i);
     return -1;
   }
   if (var.size == 1)
@@ -84,14 +90,14 @@ int var_get(Process *me, unsigned long var_id)
 // Waring: not thread-safe-or-whatever-it-is-called-in-the-mpi-world
 int var_set(Process *me, unsigned long var_id, int new_value)
 {
-  unsigned long i = var_id & 0xFF;
+  unsigned long i = vid_part(var_id);
   if (i > me->size) {
-    fprintf(stderr, "Variable requested with wrong var_id: process %lu, %luth variable\n", var_id >> 24, i);
+    fprintf(stderr, "Variable requested with wrong var_id: process %lu, %luth variable\n", pid_part(var_id), i);
     return 0;
   }
   Variable var = me->data[i];
   if (!var.data) {
-    fprintf(stderr, "Variable requested for already freed vaiable: process %lu, %luth variable\n", var_id >> 24, i);
+    fprintf(stderr, "Variable requested for already freed vaiable: process %lu, %luth variable\n", pid_part(var_id), i);
     return 0;
   }
 
@@ -101,13 +107,13 @@ int var_set(Process *me, unsigned long var_id, int new_value)
 
 void var_free(Process *me, unsigned long var_id)
 {
-  unsigned long i = var_id & 0xFF;
+  unsigned long i = vid_part(var_id);
   if (i > me->size) {
-    fprintf(stderr, "Variable free requested with wrong var_id: process %lu, %luth variable\n", var_id >> 24, i);
+    fprintf(stderr, "Variable free requested with wrong var_id: process %lu, %luth variable\n", pid_part(var_id), i);
     return;
   }
   if (!me->data[i].data) {
-    fprintf(stderr, "Variable free requested for already freed vaiable: process %lu, %luth variable\n", var_id >> 24, i);
+    fprintf(stderr, "Variable free requested for already freed vaiable: process %lu, %luth variable\n", pid_part(var_id), i);
     return;
   }
   Variable *var = me->data + i;
@@ -117,7 +123,7 @@ void var_free(Process *me, unsigned long var_id)
 
 void print_var_id(unsigned long var_id)
 {
-  printf("Process %lu, %luth variable\n", var_id >> 24, var_id & 0xFF);
+  printf("Process %lu, %luth variable\n", pid_part(var_id), vid_part(var_id));
 }
 
 
@@ -153,6 +159,7 @@ int main(int argc, char **argv)
   if (me->id == 2)
   {
     unsigned long var_id = var_alloc(me);
+    print_var_id(var_id);
     var_set(me, var_id, 42);
     printf("Value: %d\n", var_get(me, var_id));
     var_set(me, var_id, 70);
